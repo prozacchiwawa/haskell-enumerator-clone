8c2886d3f6531de4e9a2bbda0de3c4e1e4e5f3bd
John Millikin <jmillikin@gmail.com>
2010-08-21 03:13:14 +0000

    Use a decoder enumeratee in the wc example, for decoding text.

--- Examples/wc.hs
+++ Examples/wc.hs
@@ -9,16 +9,15 @@
 -----------------------------------------------------------------------------
 module Main (main) where
 import Data.Enumerator
-import Data.Enumerator.IO
+import qualified Data.Enumerator.IO as EIO
+import qualified Data.Enumerator.Text as ET
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as B8
 import qualified Data.Text as T
-import qualified Data.Text.Encoding as T
 
 -- support imports
 import Control.Exception as E
 import Data.List
-import Data.Bits ((.&.))
 import Control.Monad (unless, forM_)
 import System.IO
 import System.Console.GetOpt
@@ -52,24 +51,14 @@ iterLines = liftFoldL' step 0 where
 --
 -- Since it's possible for the input file's encoding to be non-UTF8, a bit
 -- of error handling is also required.
+--
+-- Note the use of joinI. 'ET.decode' is an enumeratee, which means it returns
+-- an iteratee yielding an inner step. 'joinI' "collapses" an enumeratee's
+-- return value, much as 'join' does to monadic values.
 
 iterChars :: Monad m => Iteratee E.SomeException B.ByteString m Integer
-iterChars = continue (step (B.empty, 0)) where
-	step accT chunk = case chunk of
-		EOF -> let (extra, acc) = accT in yield acc (Chunks [extra])
-		(Chunks xs) -> case foldl' foldStep (Just accT) xs of
-			Just accT' -> continue $ step $! accT'
-			Nothing -> throwError (E.SomeException (E.ErrorCall "Invalid UTF-8"))
-	
-	-- The 'decodeUtf8' function is complicated, and defined later, but
-	-- all it does is decode as much input as possible, then return any
-	-- remaining bytes.
-	
-	foldStep Nothing _ = Nothing
-	foldStep (Just (extra, acc)) bytes = case decodeUtf8 (B.append extra bytes) of
-		Just (text, extra') -> Just (extra', acc + toInteger (T.length text))
-		Nothing -> Nothing
-
+iterChars = joinI (ET.decode ET.utf8 $$ count) where
+	count = liftFoldL' (\acc t -> acc + toInteger (T.length t)) 0
 
 main :: IO ()
 main = do
@@ -87,7 +76,7 @@ main = do
 		putStr $ filename ++ ": "
 		
 		-- see cat.hs for commented implementation of 'Data.Enumerator.IO.enumFile'
-		eitherStat <- run (enumFile filename $$ iter)
+		eitherStat <- run (EIO.enumFile filename $$ iter)
 		putStrLn $ case eitherStat of
 			Left err -> "ERROR: " ++ show err
 			Right stat -> show stat
@@ -120,23 +109,3 @@ getMode = do
 		exitFailure
 	
 	return (Prelude.head options, files)
-
--- Incremental UTF-8 validator / decoder
-
-decodeUtf8 :: B.ByteString -> Maybe (T.Text, B.ByteString)
-decodeUtf8 allBytes = loop B.empty allBytes where
-	
-	loop acc bytes | B.null bytes = Just (T.decodeUtf8 acc, bytes)
-	loop acc bytes = do
-		let x0 = B.index bytes 0
-		req <- required x0
-		if req > B.length bytes
-			then Just (T.decodeUtf8 acc, bytes)
-			else loop (B.append acc (B.take req bytes)) (B.drop req bytes)
-	
-	required x0
-		| x0 .&. 0x80 == 0x00 = Just 1
-		| x0 .&. 0xE0 == 0xC0 = Just 2
-		| x0 .&. 0xF0 == 0xE0 = Just 3
-		| x0 .&. 0xF8 == 0xF0 = Just 4
-		| otherwise           = Nothing
